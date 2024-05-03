2e66c1324268009c744bfef2b8fa461c225814d8
Mykhailo <m.ulanovych@gmail.com>
2024-05-03 23:54:21 +0300

    'update'

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,26 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { fetchContacts, addContacts, deleteContacts } from './operations';
+
+const initialState = { items: [], isLoading: false, error: null };
+
+const getActions = type =>
+	isAnyOf(fetchContacts[type], addContact[type], deleteContact[type]);
 
 export const contactsSlice = createSlice({
 	name: 'contacts',
-	initialState: {
-		items: [
-			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-		],
-	},
-	reducers: {
-		addContact(state, action) {
-			state.items = [...state.items, action.payload];
-		},
-		deleteContact(state, action) {
-			const index = state.items.findIndex(
-				contact => contact.id === action.payload
-			);
-			state.items.splice(index, 1);
-		},
-	},
+	initialState,
+	extraReducers: builder =>
+		builder
+			.addCase(fetchContacts.fulfilled, (state, actions) => {
+				state.items = actions.payload;
+			})
+			.addCase(addContacts.fulfilled, (state, actions) => {
+				state.items = [...state.items, actions.payload];
+			})
+			.addCase(deleteContacts.fulfilled, (state, actions) => {
+				const index = state.items.findIndex(
+					contact => contact.id === actions.payload
+				);
+				state.items.splice(index, 1);
+			})
+			.addMatcher(getActions('pending'), state => {
+				state.isLoading = true; // Установка прапора isLoading true
+			})
+			.addMatcher(getActions('rejected'), (state, action) => {
+				state.isLoading = false;
+				state.error = action.payload;
+			})
+			.addMatcher(getActions('fulfilled'), state => {
+				state.isLoading = false;
+				state.error = null;
+			}),
 });
 
 export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
